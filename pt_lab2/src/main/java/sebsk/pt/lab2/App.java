3a82649513ee59561b95745f6663b32b95e5c365
SebSK3 <34949127+SebSK3@users.noreply.github.com>
2024-03-09 17:07:14 +0000

    [Lab2] even better ex. 1

--- pt_lab2/src/main/java/sebsk/pt/lab2/App.java
+++ pt_lab2/src/main/java/sebsk/pt/lab2/App.java
@@ -5,9 +5,11 @@ public class App {
         PrimesList primeList = new PrimesList();
 
         Thread addPrime = new Thread(new AddPrime(primeList));
-        Thread checkPrime = new Thread(new CheckPrime(primeList));
+        Thread checkPrime = new Thread(new CheckPrime(primeList, "first"));
+        Thread checkPrime2 = new Thread(new CheckPrime(primeList, "second"));
 
         checkPrime.start();
         addPrime.start();
+        checkPrime2.start();
     }
 }
--- pt_lab2/src/main/java/sebsk/pt/lab2/CheckPrime.java
+++ pt_lab2/src/main/java/sebsk/pt/lab2/CheckPrime.java
@@ -3,19 +3,21 @@ package sebsk.pt.lab2;
 
 public class CheckPrime implements Runnable {
     private final PrimesList checkPrime;
+    private final String name;
 
-    public CheckPrime(PrimesList checkPrime) {
+    public CheckPrime(PrimesList checkPrime, String name) {
         this.checkPrime = checkPrime;
+        this.name = name;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
             int prime = checkPrime.getPrime();
-            System.out.println("[CHECK] IsPrime: " + prime + " : " + isPrime(prime));
+            System.out.println("[CHECK] " + name + ": " + prime + " : " + isPrime(prime));
 
             try {
-                Thread.sleep(2000); // Simulate some processing time before consuming the next prime
+                Thread.sleep(2500); // Simulate some processing time before consuming the next prime
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
